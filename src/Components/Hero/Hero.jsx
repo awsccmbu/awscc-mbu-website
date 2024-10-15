207721f14dc0d869556b1801017a8a6273cddec7
awsccmbu <awsclubmbu@gmail.com>
2024-10-15 09:50:43 +0530

    removed hero image

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,12 +5,9 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 // import Button from 'react-bootstrap/Button';
-import { Parallax } from 'react-scroll-parallax';
 
 import cloud from '../../assets/img/cloud.png';
 import cloud1 from '../../assets/img/cloud1.png';
-
-import Chennai from '../../assets/img/chennai.png';
 
 function Hero() {
   const [scrollOffset, setScrollOffset] = useState(0);
@@ -53,13 +50,6 @@ function Hero() {
               <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1" />
             </svg>
           </Button> */}
-        </Col>
-      </Row>
-      <Row>
-        <Col className="hero-img-container d-flex justify-content-end align-items-end">
-          <Parallax scale={1.5}>
-            <img src={Chennai} alt="mbu-logo" className="" />
-          </Parallax>
         </Col>
       </Row>
     </Container>
